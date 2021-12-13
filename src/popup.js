ec47231b9dccacf3121d9f36f782774c617d3780
Josiah McClurg <jcmcclurg@gmail.com>
2021-12-13 02:23:12 -0500

    Squashing bugs. Added a download all

--- src/popup.js
+++ src/popup.js
@@ -1,49 +1,73 @@
+
 document.addEventListener('DOMContentLoaded', function() {
-//  var checkPageButton = document.getElementById('findPhotos');  <button id="findPhotos">Find all photos</button>
-//  checkPageButton.addEventListener('click', function() {
+   //  var checkPageButton = document.getElementById('findPhotos');  <button id="findPhotos">Find all photos</button>
+   //  checkPageButton.addEventListener('click', function() {
 
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 
-"	var d = document, arr=[];" + 
-"	var imgs = d.getElementsByTagName('gv-image-attachment'); " +
-"	for(var i in imgs) { if (!imgs[i].getElementsByClassName) continue; " +
-"		photoDivs = imgs[i].getElementsByClassName('md-body-1'); " +
-"		for(var j in photoDivs) { if (!photoDivs[j].getElementsByTagName) continue;" +
-"			if (photoDivs[j].getElementsByTagName('img').length>0)" +
-"		        	arr.push(photoDivs[j].getElementsByTagName('img')[0].src);" +
-"			else if (photoDivs[j].getElementsByTagName('div').length>0)" +
-"				arr.push('https://' + window.location.hostname + photoDivs[j].getElementsByTagName('div')[0].style.backgroundImage.split('\"')[1].split('?')[0] );" +
-"	}; }; " + 
-"	arr "
+   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.executeScript( tabs[0].id, {file: 'script.js'}, function (result) {
+         if (result.length==0) {
+            document.getElementById('msg').innerHTML = "No photos found"
+         }
+         else {
+            document.getElementById('msg').innerHTML = result[0].length + " photos found. Click a thumbnail to download individual photos."
 
-      }, function (result) {
-      	if (result.length==0) {
-      		document.getElementById('msg').innerHTML = "No photos found";
-      	}
-      	else {
-      		document.getElementById('msg').innerHTML = "Click a photo to download instantly:";
-		var skipd = 0;	// number of own uploaded imgs
-		for (i = 0; i < result[0].length; i++) {
-		    if (result[0][i].endsWith('jpg')) {
-		    	skipd = skipd + 1;
-		    	continue;
-		    }
-		    var link = document.createElement('a');
-		    link.href = result[0][i] + '?s=4';	// it's a div
-		    link.download = 'gvoice-'+i;
-		    var ig = new Image(); ig.src=result[0][i]; ig.style.maxHeight='128px'; ig.style.maxWidth='128px';
-		    link.appendChild(ig);
-		    document.body.appendChild(link);
-		};
-		if (skipd>0) {
-			var t = document.createTextNode('Skipped '+skipd+' uploaded photos');
-			document.body.appendChild(t);
-		};
-	};
-      });
-    });
+            rslt = result[0]
+            downloadList = [];
 
-}, false);
+            for (i = 0; i < rslt.length; i++) {
+               // Read in date string as a date object, then reformat it to
+               // something a bit more standard. We are doing this work here
+               // because we have loaded the dayjs library in this script.
+               rslt[i].title = rslt[i].datestr
+               rslt[i].datestr = dayjs(rslt[i].datestr, 'MMMM DD YYYY H:mm A').format('YYYY-MM-DD')
+               rslt[i].filename = rslt[i].datestr + '.jpg'
+               downloadList.push({url: rslt[i].url, filename: rslt[i].filename, conflictAction: "uniquify"})
+            }
+            console.log(rslt)
+
+            // I tried to download them all in parallel, but it seemed like
+            // Google was detecting this as a potential attack, and was actively
+            // preventing it. So, I switched to downloading them sequentially.
+            var link = document.createElement('a')
+            link.href = "#"
+            link.title = 'Download All Photos'
+            link.text = 'Download All'
+            link.onclick = function(){
+               downloadSequentially(downloadList, () => console.log('Download completed.'));
+               return false
+            }
+            document.body.appendChild(link)
+
+
+            // As of Dec 2021, the commented-out code is not needed as far as I can tell.
+            // var skipd = 0;   // number of own uploaded imgs
+            for (i = 0; i < rslt.length; i++) {
+               //if (result[0][i].endsWith('jpg')) {
+               //   skipd = skipd + 1
+               //   continue
+               //}
+               
+               var l = document.createElement('a')
+               l.id = "download-photo-google-voice-" + i;
+
+               l.href = rslt[i].url
+               l.title = rslt[i].title
+               // As of Dec 2021, the download option of <a> is only a very gentle suggestion. The actual work is done via the download api in the onclick function below.
+               l.download = rslt[i].filename
+               l.onclick = function(){
+                  chrome.downloads.download({url: this.href, filename: this.download, conflictAction: "uniquify"},function(id) {})
+                  return false
+               }
+               var ig = new Image(); ig.src=rslt[i].thumb; ig.style.maxHeight='128px'; ig.style.maxWidth='128px'
+               l.appendChild(ig)
+               document.body.appendChild(l)
+            }
+            //if (skipd>0) {
+            //   var t = document.createTextNode('Skipped '+skipd+' uploaded photos')
+            //   document.body.appendChild(t)
+            //}
+         }
+      })
+   })
+}, false)
 
